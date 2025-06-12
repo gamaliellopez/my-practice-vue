b2fc708afce816f8bdf7d2d5d3cd089305cabc1c
unknown <lopezortizgamaliel@gmail.com>
2025-06-11 23:33:59 -0600

    se agrego boostrap

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <h1 class="text-primary mb-4">Mi Lista de Tareas</h1>
+    
+    <div class="input-group mb-3">
+      <input v-model="newTask" type="text" class="form-control" placeholder="Nueva tarea" @keyup.enter="addTask" />
+      <button class="btn btn-primary" @click="addTask">Agregar</button>
+    </div>
+
+    <ul class="list-group">
+      <li v-for="(task, index) in tasks" :key="index" class="list-group-item d-flex justify-content-between align-items-center">
+        {{ task }}
+        <button class="btn btn-danger btn-sm" @click="removeTask(index)">Eliminar</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newTask: '',
+      tasks: []
+    }
+  },
+  methods: {
+    addTask() {
+      if(this.newTask.trim() !== '') {
+        this.tasks.push(this.newTask.trim())
+        this.newTask = ''
+      }
+    },
+    removeTask(index) {
+      this.tasks.splice(index, 1)
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
