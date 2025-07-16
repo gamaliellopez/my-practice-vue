3de2b5e206fdae013608bd3d7dc0d415b8bba972
unknown <lopezortizgamaliel@gmail.com>
2025-07-15 22:04:49 -0600

    Cambios realizados en mi-todo-app

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,16 @@
 <template>
-  <div class="container mt-5">
-    <h1 class="text-primary mb-4">Mi Lista de Tareas</h1>
-    
-    <div class="input-group mb-3">
-      <input v-model="newTask" type="text" class="form-control" placeholder="Nueva tarea" @keyup.enter="addTask" />
-      <button class="btn btn-primary" @click="addTask">Agregar</button>
-    </div>
-
-    <ul class="list-group">
-      <li v-for="(task, index) in tasks" :key="index" class="list-group-item d-flex justify-content-between align-items-center">
-        {{ task }}
-        <button class="btn btn-danger btn-sm" @click="removeTask(index)">Eliminar</button>
-      </li>
-    </ul>
+  <div>
+    <h1>Rick and Morthy</h1>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      newTask: '',
-      tasks: []
-    }
-  },
-  methods: {
-    addTask() {
-      if(this.newTask.trim() !== '') {
-        this.tasks.push(this.newTask.trim())
-        this.newTask = ''
-      }
-    },
-    removeTask(index) {
-      this.tasks.splice(index, 1)
-    }
+  name: 'App',
+  components: {
+
   }
 }
 </script>
+<style>
+</style>
