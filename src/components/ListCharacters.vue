ef74110b4a75b328fee29d709d7a82f6a8498cf3
unknown <lopezortizgamaliel@gmail.com>
2025-07-15 22:39:12 -0600

    cambios de prueba

---
+++ src/components/ListCharacters.vue
@@ -0,0 +1,20 @@
+<template>
+<section>
+    <div class="characters">
+        <div class="item_characters">
+<h2>characters</h2>
+        </div>
+
+
+    </div>
+</section>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style></style>
